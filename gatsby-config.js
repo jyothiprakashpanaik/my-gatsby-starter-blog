bd0f98e69e34f55d125268a5d62d34124b13398e
jyothiprakashpatnaik <jyothiprakashpatnaik@gmail.com>
2020-10-17 00:47:52 +0530

    Added initial post

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,21 +4,21 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: `gatsby-scource-filesystem`,
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: 'pages',
         path: `${__dirname}/src/pages`
       },
     },
     {
-      resolve: `gatsby-scource-filesystem`,
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: 'posts',
         path: `${__dirname}/src/posts`
       },
     },
     {
-      resolve: `gatsby-scource-filesystem`,
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: 'images',
         path: `${__dirname}/src/images`
